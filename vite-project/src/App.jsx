23a70724f632d9e4182e72d5df3b5de5bfec8fa9
Alfredo <kmstry214@gmail.com>
2025-01-15 19:56:57 -0600

    refactor input

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,42 +1,66 @@
-import "./App.css";
-
-import Form from "./components/Form/Form";
-import Resume from "./components/Resume/Resume";
-import educationData from "./components/DummyData/EducationData";
-import experienceData from "./components/DummyData/ExperienceData";
-import personalData from "./components/DummyData/PersonalData";
 import { useState } from "react";
 
 function App() {
-  const [personalInfo, setPersonalInfo] = useState(personalData);
-  const [experienceInfo, setExperienceInfo] = useState(experienceData);
-  const [educationInfo, setEducationInfo] = useState(educationData);
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [inputs, setInputs] = useState({});
 
-  // const [isEditing, setIsEditing] = useState(false);
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted form", inputs);
+    setInputs("");
+  };
+
+  const inputFields = [
+    {
+      label: "First Name",
+      name: "firstName",
+      placeholder: "First Name",
+      type: "text",
+    },
+    {
+      label: "Last Name",
+      name: "lastName",
+      placeholder: "Last Name",
+      type: "text",
+    },
+    {
+      label: "Email",
+      name: "email",
+      placeholder: "example@gmail.com",
+      type: "email",
+    },
+    {
+      label: "Phone",
+      name: "phone",
+      placeholder: "###-###-####",
+      type: "tel",
+    },
+  ];
+
+  const renderInputs = inputFields.map((field, i) => {
+    return (
+      <div key={i}>
+        <label>{field.label}</label>
+        <input
+          type={field.type}
+          name={field.name}
+          placeholder={field.placeholder}
+          value={inputs[field.name] || ""}
+          onChange={inputHandler}
+        />
+      </div>
+    );
+  });
 
   return (
-    <div className="main-container">
-      <div className="form-container">
-        <Form
-          personalInfo={personalInfo}
-          setPersonalInfo={setPersonalInfo}
-          experienceInfo={experienceInfo}
-          setExperienceInfo={setExperienceInfo}
-          educationInfo={educationInfo}
-          setEducationInfo={setEducationInfo}
-          isSubmitted={isSubmitted}
-          setIsSubmitted={setIsSubmitted}
-        />
-      </div>
-      <div className="resume-container">
-        <Resume
-          personalInfo={personalInfo}
-          experienceInfo={experienceInfo}
-          educationInfo={educationInfo}
-        />
-      </div>
-    </div>
+    <form onSubmit={handleSubmit}>
+      {renderInputs}
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
--- vite-project/src/components/DummyData/PersonalData.jsx
+++
@@ -1,7 +0,0 @@
-const personalData = [
-  { id: 0, type: "text", value: "", name: "fullName", label: "Full Name" },
-  { id: 1, type: "email", value: "", name: "email", label: "Email" },
-  { id: 2, type: "tel", value: "", name: "phone", label: "Phone" },
-];
-
-export default personalData;
